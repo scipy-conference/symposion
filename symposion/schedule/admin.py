5e65d185681f52a1a34eca3b49208ff6c43fd822
codersquid <github@codersquid.com>
2014-05-08 13:39:57 -0500

    improve admin display for schedule

--- symposion/schedule/admin.py
+++ symposion/schedule/admin.py
@@ -6,23 +6,26 @@ from symposion.schedule.models import Schedule, Day, Room, SlotKind, Slot, SlotR
 admin.site.register(Schedule)
 admin.site.register(
     Day,
-    list_display=("date", "schedule"),
-    list_filter=("schedule"),
+    list_display=("date", "schedule")
 )
 admin.site.register(
     Room,
     list_display=("name", "order", "schedule")
 )
-admin.site.register(SlotKind)
+admin.site.register(
+    SlotKind,
+    list_display=("label", "schedule")
+)
 admin.site.register(
     Slot,
     list_display=("day", "start", "end", "kind")
 )
 admin.site.register(
     SlotRoom,
-    list_display=("slot", "room")
+    list_display=("room", "slot")
 )
 admin.site.register(
     Presentation,
+    list_display=("title", "speaker", "section", "number"),
     list_filter=("section", "slot", "cancelled")
 )
