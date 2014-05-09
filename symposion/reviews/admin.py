e2196c75c4f1f558aca0de6b58d2b98de9965707
codersquid <github@codersquid.com>
2014-05-09 15:44:55 -0500

    add ProposalResult to admin

--- symposion/reviews/admin.py
+++ symposion/reviews/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from symposion.reviews.models import NotificationTemplate, Review
+from symposion.reviews.models import NotificationTemplate, Review, ProposalResult
 
 admin.site.register(Review,
         list_display = ['proposal', 'vote', 'user'],
         list_filter = ['vote',],
         date_heirarchy='submitted_at',
         )
-
-
-
 admin.site.register(NotificationTemplate)
+admin.site.register(ProposalResult,
+        list_display=["proposal", "status"],
+        list_filter=["status"],
+)
