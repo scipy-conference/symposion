cf5e6c81b80dbf6f420c0c682e5c76b69eeddb31
Sheila Miguez <github@codersquid.com>
2014-06-10 18:16:32 -0500

    add presentation to admin for SlotKind and add some admin filters

--- symposion/schedule/admin.py
+++ symposion/schedule/admin.py
@@ -11,15 +11,18 @@ admin.site.register(
 )
 admin.site.register(
     Room,
-    list_display=("name", "order", "schedule")
+    list_display=("name", "order", "schedule"),
+    list_filter=("schedule")
 )
 admin.site.register(
     SlotKind,
-    list_display=("label", "schedule")
+    list_display=("label", "schedule", "presentation"),
+    list_filter=("presentation")
 )
 admin.site.register(
     Slot,
-    list_display=("day", "start", "end", "kind")
+    list_display=("day", "start", "end", "kind"),
+    list_filter=("kind")
 )
 admin.site.register(
     SlotRoom,
